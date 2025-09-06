6cb4915bfe038b62ec62831d1fc35b74adb537c4
Chirag Jeevanani <144720020+chiragjeevanani@users.noreply.github.com>
2025-09-06 21:24:31 +0530

    Update Icons.tsx

--- components/icons/Icons.tsx
+++ components/icons/Icons.tsx
@@ -140,3 +140,18 @@ export const SpinnerIcon: React.FC<IconProps> = (props) => (
     <path d="M21 12a9 9 0 1 1-6.219-8.56" />
   </svg>
 );
+
+export const CooktopIcon: React.FC<IconProps> = (props) => (
+  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+    <circle cx="12" cy="12" r="1" />
+    <path d="M12 6V4" />
+    <path d="m16.24 7.76 1.42-1.42" />
+    <path d="M18 12h2" />
+    <path d="m16.24 16.24 1.42 1.42" />
+    <path d="M12 18v2" />
+    <path d="m7.76 16.24-1.42 1.42" />
+    <path d="M6 12H4" />
+    <path d="m7.76 7.76-1.42-1.42" />
+    <path d="M12 17a5 5 0 0 0 5-5h-5Z" />
+  </svg>
+);
